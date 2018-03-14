376a599218fb4f0b783b939f11b200039e0231ce
Dipen-Dedania <dipen@zuru.tech>
2018-03-14 18:53:09 +0530

    face detection with Android app

---
+++ 24_opencv_android_mobile_face_detection.py
@@ -0,0 +1,39 @@
+import urllib.request
+import cv2
+import numpy as np
+import time
+
+# Replace the URL with your own IPwebcam IP:port
+url='http://192.168.42.129:8080/shot.jpg'
+
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+face_cascade = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_default.xml')
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
+eye_cascade = cv2.CascadeClassifier('Cascades/haarcascade_eye.xml')
+
+while True:
+    # Use urllib to get the image and convert into a cv2 usable format
+    imgResp=urllib.request.urlopen(url)
+    imgNp=np.array(bytearray(imgResp.read()),dtype=np.uint8)
+    img=cv2.imdecode(imgNp,-1)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    # put the image on screen
+    cv2.imshow('IPWebcam',img)
+
+    #To give the processor some less stress
+    #time.sleep(0.1)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
